3887b89b41e3bb5317f715f945d5e0276ce38e9c
Cameron Baumgardner <31215393+cbaumgardner@users.noreply.github.com>
2022-03-03 18:13:10 -0500

    Making selections sticky

--- app.py
+++ app.py
@@ -14,9 +14,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def home():
+    state = ""
     if request.method == "POST":
         key_note_input = request.form["key_note_select"]
         scale_type_input = request.form["scale_type_select"]
+        state = {'scale_choice': scale_type_input, 'key_choice': key_note_input}
         fretboard = FretBoard(key_note=key_note_input, scale_type=scale_type_input)
     else:
         fretboard = FretBoard()
@@ -24,7 +26,8 @@ def home():
     return render_template("index.html",
                            key_note_data=key_notes,
                            scale_type_data=scale_types,
-                           tab=fretboard.draw_neck())
+                           tab=fretboard.draw_neck(),
+                           state=state)
 
 
 if __name__ == "__main__":
